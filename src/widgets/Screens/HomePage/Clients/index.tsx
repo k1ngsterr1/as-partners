96ee8a3b7ebdae45f55c401983e882f56368c9df
Ruslan Makhmatov <ruslanmakhmatov@gmail.com>
2024-01-15 18:07:23 +0600

    Version 1.0.8

---
+++ src/widgets/Screens/HomePage/Clients/index.tsx
@@ -0,0 +1,21 @@
+import { Separator } from "@shared/ui/Separator";
+
+import styles from "./styles.module.scss";
+
+export const ClientsScreen = () => {
+  return (
+    <div className="w-full bg-white">
+      <section className="container">
+        <h5 className={styles.heading}>Клиенты</h5>
+        <Separator marginTop="mt-4" />
+        <p className="paragraph mt-4">
+          Добро пожаловать на сайт AS Partners Architect, вашего надежного
+          партнера в мире современной архитектуры и дизайна. Мы специализируемся
+          на создании инновационных, функциональных и эстетически
+          привлекательных архитектурных решений, которые отражают последние
+          тенденции в области урбанистики и дизайна интерьеров.
+        </p>
+      </section>
+    </div>
+  );
+};
