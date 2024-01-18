8ba249bbb85463f2c8db00befacacff1bb91fbe5
Ruslan Makhmatov <ruslanmakhmatov@gmail.com>
2024-01-18 22:10:11 +0600

    Version 1.1.9

--- src/widgets/Screens/AboutPage/Main/index.tsx
+++ src/widgets/Screens/AboutPage/Main/index.tsx
@@ -1,5 +1,6 @@
 import { BurgerButton } from "@features/BurgerButton/ui";
 import { Breadcrumbs } from "@shared/ui/Breadcrumbs";
+import { Button } from "@shared/ui/Button";
 import { Separator } from "@shared/ui/Separator";
 import { UnderText } from "@shared/ui/UnderText";
 
@@ -14,8 +15,15 @@ export const AboutMainScreen = () => {
       <BurgerButton color="black_bg" />
       <h1 className="black">AS & Partners Architect</h1>
       <Separator marginTop="mt-4" />
-      <UnderText label="ИНФОРМАЦИЯ О НАС" marginTop="mt-4" />
+      <UnderText label="ИНФОРМАЦИЯ О НАС" marginTop="mt-6" />
       <Breadcrumbs crumbs={breadcrumbData} marginTop="mt-4" />
+      <p className="paragraph_black text-center mt-5">
+        Одним из направлений работы нашей студии является проектирование
+        коттеджных поселков разного масштаба и типологии. В работе мы применяем
+        наш авторский подход, сформированный многолетним опытом в сфере
+        архитектуры и дизайна
+      </p>
+      <Button text="Подробнее" margin="mt-8" />
     </main>
   );
 };
