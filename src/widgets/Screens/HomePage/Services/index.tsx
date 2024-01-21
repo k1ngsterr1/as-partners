7044b12158d7ca211b6d03dd938406e87c7d6584
Ruslan Makhmatov <ruslanmakhmatov@gmail.com>
2024-01-21 18:05:45 +0600

    Version 1.3.6

--- src/widgets/Screens/HomePage/Services/index.tsx
+++ src/widgets/Screens/HomePage/Services/index.tsx
@@ -1,8 +1,10 @@
 import { ServiceTab } from "@entities/ServiceTab";
 import { Separator } from "@shared/ui/Separator";
+import { Button } from "@shared/ui/Button";
+
+import image from "@assets/main/about_block_image.webp";
 
 import styles from "./styles.module.scss";
-import { Button } from "@shared/ui/Button";
 
 export const ServicesScreen = () => {
   return (
@@ -21,6 +23,37 @@ export const ServicesScreen = () => {
         <ServiceTab text="Пример проекта" margin="mt-8" />
         <ServiceTab text="Пример проекта" margin="mt-8" />
         <Button text="Все услуги" margin="mt-8 mb-8" />
+      </section>
+      <section className="container-pc">
+        <div className={styles.section_one}>
+          <img
+            className={`${styles.image} mt-8`}
+            src={image}
+            alt="service_image"
+          />
+          <img
+            className={`${styles.image} mt-8`}
+            src={image}
+            alt="service_image"
+          />
+          <img
+            className={`${styles.image} mt-8`}
+            src={image}
+            alt="service_image"
+          />
+        </div>
+        <div className={styles.section_two}>
+          <h3 className={styles.heading}>Наши Услуги</h3>
+          <Separator marginTop="mt-5 mr-8" />
+          <p className="paragraph black !text-right  mt-8 mr-8">
+            Добро пожаловать на сайт AS Partners Architect, вашего надежного
+            партнера в мире современной архитектуры и дизайна. Мы
+            специализируемся на создании инновационных, функциональных и
+            эстетически привлекательных архитектурных решений, которые отражают
+            последние тенденции в области урбанистики и дизайна интерьеров.
+          </p>
+          <Button text="Все услуги" margin="mt-8 mr-8" />
+        </div>
       </section>
     </div>
   );
