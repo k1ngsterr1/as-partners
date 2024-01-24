e14e76ac246db66326cd4adf174532a93dd6b879
Chechen Daddy <ruslanmakhmatov@gmail.com>
2024-01-24 13:06:06 +0600

    Version 1.6.1

--- src/entities/Header/index.tsx
+++ src/entities/Header/index.tsx
@@ -1,11 +1,14 @@
 import logo from "@assets/main/logo.svg";
-
-import styles from "./styles.module.scss";
+import { useNavigate } from "react-router-dom";
 import { MenuButton } from "@shared/ui/MenuButton";
 
+import styles from "./styles.module.scss";
+
 export const Header = () => {
+  const navigate = useNavigate();
+
   return (
-    <header className={styles.header}>
+    <header className={styles.header} onClick={() => navigate("/home")}>
       <img src={logo} className={styles.header__logo} alt="logo" />
       <MenuButton />
     </header>
